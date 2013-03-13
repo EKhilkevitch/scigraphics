bf9bfdb3e90050708c91b2b5531c84b7cc63944b
Evgeniy Khilkevitch <evgeniy-hila@yandex.ru>
2013-03-13 12:41:03 +0400

    Fix exampleAnimation for windows

--- source/example/exampleAnimation.cpp
+++ source/example/exampleAnimation.cpp
@@ -31,6 +31,35 @@
 
 // ======================================================
 
+const unsigned TimerInterval = 20;
+
+// ======================================================
+
+class timerThread : public QThread
+{
+  private:
+    plotAnimator *Animator;
+    bool NeedToStop;
+
+    void run()
+    {
+      do
+      {
+	QTime Timer;
+	Timer.start();
+	while ( Timer.elapsed() < TimerInterval && !NeedToStop )
+	  msleep(1);
+	QMetaObject::invokeMethod( Animator, "updateGraphics", Qt::QueuedConnection );
+      } while ( ! NeedToStop );
+    }
+
+  public:
+    timerThread( plotAnimator *A ) : Animator(A), NeedToStop(false) {}
+    void stop() { NeedToStop = true; }
+};
+
+// ======================================================
+
 int main( int argc, char **argv )
 {
   QApplication app(argc,argv);
@@ -43,18 +72,27 @@ int main( int argc, char **argv )
   Plot.show();
   Plot.replot();
 
+#if 0 
+  // Easy way to do animation, but on Windows this is very slow.
   QTimer Timer;
   QObject::connect( &Timer, SIGNAL(timeout()), &Animator, SLOT(updateGraphics()) );
-  Timer.start(20);
+  Timer.start(TimerInterval);
+#else
+  // Difficult way to do animation, but no Windows this code is fast.
+  timerThread Thread(&Animator);
+  Thread.start();
+#endif
   
   QTime WorkTime;
   WorkTime.start();
 
   int RetCode = app.exec();
 
-  qDebug() << "FPS = " << (double)Animator.count() / ( 1e-3 * WorkTime.elapsed() ) << " Max is " << 1.e3/Timer.interval();
+  qDebug() << "FPS = " << (double)Animator.count() / ( 1e-3 * WorkTime.elapsed() ) << " Max is " << 1.e3/TimerInterval;
   
-
+  Thread.stop();
+  while ( Thread.isRunning() ) { }
+  
   return RetCode;
 }
 
